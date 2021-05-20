fe2723ac33b8b3df6b60580d76f3b276fbfe5788
pradyumna surampudi <p.surampudi@addlab.it>
2021-05-20 18:21:04 +0530

    updated service select

--- src/Pages/Booking/Services.js
+++ src/Pages/Booking/Services.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import {selectService, serviceList} from '../../Store/Actions';
+import { useForm } from 'react-hook-form';
 
 // const test_services = [
 //     {
@@ -27,18 +28,25 @@ import {selectService, serviceList} from '../../Store/Actions';
 const Services = () => {
     const dispatch = useDispatch();
     const test_services = useSelector((state) => state.booking.serviceList)
-
-    dispatch(selectService("hello"));
-
-    // Replace with api response
+    const defaultService = useSelector((state)=> state.booking.selectedService)
+    const { register, handleSubmit, errors } = useForm();
+    const onSubmit = (data) => {
+        
+        console.log(data);
+        dispatch(selectService(data));
+    }
+    console.log(errors);
+    // Replace with api response on load of component
     // dispatch(selectService(selectedService));
     // dispatch(serviceList(response.data.serviceList));
+
     return (
         <>
         <div className="col-span-6 shadow-2xl p-8 row-span-9 overflow-y-auto rounded-t-xl h-full bg-red-50 relative">
             <div className="w-full h-auto" >
                 <h1 className="text-main font-bold text-lg pb-4 pt-2 ">1. Select your service</h1>
             </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
             <div className="services_wrapper mt-5 ml-4">
             {test_services.map((category) => (
                     <section className="w-full mt-4 mb-8">
@@ -51,9 +59,11 @@ const Services = () => {
                                             <h4 htmlFor={service.id} className="text-main font-bold text-sm inline cursor-pointer w-full">€ {service.price}</h4>
                                             <input
                                             id={service.id}
-                                            name="group1"
+                                            name="serviceGroup"
                                             type="radio"
+                                            value={service.id}
                                             className="focus:text-main h-5 w-5 top-1/3 text-main border-gray-300 absolute right-5"
+                                            {...register('serviceGroup',{ required: true })}
                                             />
                                             <p className="text-gray-500 text-sm pt-2">
                                                 {service.desription}
@@ -62,9 +72,10 @@ const Services = () => {
                                     ))}
                     </section>
                        ))}
+                       
             </div>
-            <button className="text-white bg-main rounded px-16 py-2 text-sm fixed right-12 bottom-12">Select</button>
-
+            <input className="text-white bg-main rounded px-16 py-2 text-sm fixed right-12 bottom-12" type="submit" value="Select" />
+            </form>
         </div>
         </>
     )
